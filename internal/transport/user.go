d0c2b01fc897d74c9692b5dbdfb4a2ad54b9fab0
Maria Meshcheryakova <mme.06072003@gmail.com>
2025-03-10 22:40:14 +0300

    restore name and surname validation (#39)

--- internal/transport/user.go
+++ internal/transport/user.go
@@ -11,7 +11,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"regexp"
-	"strings"
 	"time"
 )
 
@@ -19,6 +18,7 @@ import (
 
 var (
 	emailRegexp     = regexp.MustCompile(`^\w+(\.\w*)*@\w+(\.\w{2,})+$`)
+	nameRegexp      = regexp.MustCompile(`^[a-zA-Zа-яА-ЯёЁ\s-]+$`)
 	digitRegexp     = regexp.MustCompile(`[0-9]`)
 	lowercaseRegexp = regexp.MustCompile(`[a-z]`)
 	uppercaseRegexp = regexp.MustCompile(`[A-Z]`)
@@ -50,17 +50,17 @@ func NewAuthHandler(repo IUserRepository, log *logrus.Logger, token ITokenator) 
 	}
 }
 
-//	@Summary		Login user
-//	@Description	Авторизация пользователя
-//	@Tags			auth
-//	@Accept			json
-//	@Produce		json
-//	@Param			request	body		models.UserLoginRequestDTO	true	"User credentials"
-//	@success		200		{}			-							"No Content"
-//	@Header			200		{string}	Set-Cookie					"Устанавливает JWT-токен в куки"
-//	@Failure		400		{object}	utils.ErrorResponse			"Ошибка валидации"
-//	@Failure		401		{object}	utils.ErrorResponse			"Неверные email или пароль"
-//	@Router			/auth/login [post]
+// @Summary			Login user
+// @Description		Авторизация пользователя
+// @Tags			auth
+// @Accept			json
+// @Produce			json
+// @Param			request	body		models.UserLoginRequestDTO	true	"User credentials"
+// @success			200		{}			-							"No Content"
+// @Header			200		{string}	Set-Cookie					"Устанавливает JWT-токен в куки"
+// @Failure			400		{object}	utils.ErrorResponse			"Ошибка валидации"
+// @Failure			401		{object}	utils.ErrorResponse			"Неверные email или пароль"
+// @Router			/auth/login [post]
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	var request models.UserLoginRequestDTO
 	if errStatusCode, errMessage := utils.ParseData(r.Body, &request); errStatusCode != 0 && errMessage != "" {
@@ -100,18 +100,18 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	utils.SendSuccessResponse(w, http.StatusOK, nil)
 }
 
-//	@Summary		Register user
-//	@Description	Создает нового пользователя, хеширует пароль и устанавливает JWT-токен в куки
-//	@Tags			auth
-//	@Accept			json
-//	@Produce		json
-//	@Param			input	body		models.UserRegisterRequestDTO	true	"Данные для регистрации"
-//	@success		200		{}			-								"No Content"
-//	@Header			200		{string}	Set-Cookie						"Устанавливает JWT-токен в куки"
-//	@Failure		400		{object}	utils.ErrorResponse				"Некорректный запрос"
-//	@Failure		409		{object}	utils.ErrorResponse				"Пользователь уже существует"
-//	@Failure		500		{object}	utils.ErrorResponse				"Внутренняя ошибка сервера"
-//	@Router			/auth/register [post]
+// @Summary			Register user
+// @Description		Создает нового пользователя, хеширует пароль и устанавливает JWT-токен в куки
+// @Tags			auth
+// @Accept			json
+// @Produce			json
+// @Param			input	body		models.UserRegisterRequestDTO	true	"Данные для регистрации"
+// @success			200		{}			-								"No Content"
+// @Header			200		{string}	Set-Cookie						"Устанавливает JWT-токен в куки"
+// @Failure			400		{object}	utils.ErrorResponse				"Некорректный запрос"
+// @Failure			409		{object}	utils.ErrorResponse				"Пользователь уже существует"
+// @Failure			500		{object}	utils.ErrorResponse				"Внутренняя ошибка сервера"
+// @Router			/auth/register [post]
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	var request models.UserRegisterRequestDTO
 	if errStatusCode, errMessage := utils.ParseData(r.Body, &request); errStatusCode != 0 && errMessage != "" {
@@ -132,6 +132,13 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	if err := validateName(request.Name); err != nil {
 		utils.SendErrorResponse(w, http.StatusBadRequest, "Invalid name")
 		return
+	}
+
+	if request.Surname.Valid {
+		if err := validateName(request.Surname.String); err != nil {
+			utils.SendErrorResponse(w, http.StatusBadRequest, "Invalid surname")
+			return
+		}
 	}
 
 	passwordHash, err := GeneratePasswordHash(request.Password)
@@ -170,12 +177,12 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	utils.SendSuccessResponse(w, http.StatusOK, nil)
 }
 
-//	@Summary		Logout user
-//	@Description	Выход пользователя
-//	@Tags			auth
-//	@Security		TokenAuth
-//	@Failure		500	{object}	utils.ErrorResponse	"Ошибка сервера"
-//	@Router			/auth/logout [post]
+// @Summary		Logout user
+// @Description	Выход пользователя
+// @Tags		auth
+// @Security	TokenAuth
+// @Failure		500	{object}	utils.ErrorResponse	"Ошибка сервера"
+// @Router		/auth/logout [post]
 func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 	userID, isExist := r.Context().Value("userID").(string)
 	if !isExist {
@@ -200,15 +207,15 @@ func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
 	utils.SendSuccessResponse(w, http.StatusOK, nil)
 }
 
-//	@Summary		Get user info
-//	@Description	Получение информации о текущем пользователе
-//	@Tags			users
-//	@Security		TokenAuth
-//	@Produce		json
-//	@Success		200	{object}	models.User			"Информация о пользователе"
-//	@Failure		401	{object}	utils.ErrorResponse	"Неверный токен"
-//	@Failure		500	{object}	utils.ErrorResponse	"Ошибка сервера"
-//	@Router			/users/me [get]
+// @Summary		Get user info
+// @Description	Получение информации о текущем пользователе
+// @Tags		users
+// @Security	TokenAuth
+// @Produce		json
+// @Success		200	{object}	models.User			"Информация о пользователе"
+// @Failure		401	{object}	utils.ErrorResponse	"Неверный токен"
+// @Failure		500	{object}	utils.ErrorResponse	"Ошибка сервера"
+// @Router		/users/me [get]
 func (h *AuthHandler) GetMe(w http.ResponseWriter, r *http.Request) {
 	userIDStr, ok := r.Context().Value("userID").(string)
 	if !ok {
@@ -268,10 +275,14 @@ func validatePassword(password string) error {
 
 // validateName проверяет валидность имени пользователя
 func validateName(name string) error {
-	name = strings.TrimSpace(name)
-	if name == "" {
-		return errors.New("name cannot be empty")
+	if len(name) < 2 || len(name) > 24 {
+		return errors.New("name must be between 2 and 24 characters long")
 	}
+
+	if !nameRegexp.MatchString(name) {
+		return errors.New("name can only contain letters, spaces, and '-'")
+	}
+
 	return nil
 }
 
