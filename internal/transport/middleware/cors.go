139bf02d5cd443a2fa721ade019323206284425d
Nikita <nmamaev49@gmail.com>
2025-04-18 22:23:54 +0300

    fix cors and fill_db

--- internal/transport/middleware/cors.go
+++ internal/transport/middleware/cors.go
@@ -1,31 +1,36 @@
 package middleware
 
 import (
-	"github.com/go-park-mail-ru/2025_1_ChillGuys/config"
+	"fmt"
 	"net/http"
+
+	"github.com/go-park-mail-ru/2025_1_ChillGuys/config"
 )
 
 func CORSMiddleware(next http.Handler, conf *config.ServerConfig) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-
-		if origin == "" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		//allowedOrigin := conf.AllowOrigin
-		allowedOrigin := "*"
-
-		if allowedOrigin != "*" && allowedOrigin != origin {
-			http.Error(w, "CORS Origin not allowed", http.StatusForbidden)
-			return
-		}
-
 		w.Header().Set("Access-Control-Allow-Methods", conf.AllowMethods)
 		w.Header().Set("Access-Control-Allow-Headers", conf.AllowHeaders)
 		w.Header().Set("Access-Control-Allow-Credentials", conf.AllowCredentials)
 		w.Header().Set("Access-Control-Allow-Origin", conf.AllowOrigin)
+		
+		origin := r.Header.Get("Origin")
+
+		fmt.Println(origin + " cors")
+		if origin == "" {
+			fmt.Println("return if origin")
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		allowedOrigin := conf.AllowOrigin
+		//allowedOrigin := "*"
+
+		if allowedOrigin != "*" && allowedOrigin != origin {
+			fmt.Println(allowedOrigin + "!=")
+			http.Error(w, "CORS Origin not allowed", http.StatusForbidden)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
