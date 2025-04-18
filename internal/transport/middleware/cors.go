a456ae2fa421cc324259ae3de55fe620786a6edc
nik-mLb <148656910+nik-mLb@users.noreply.github.com>
2025-04-18 22:32:14 +0300

    Небольшие правки (#79)
    * fix cors and fill_db
    
    * fix cors

--- internal/transport/middleware/cors.go
+++ internal/transport/middleware/cors.go
@@ -1,8 +1,9 @@
 package middleware
 
 import (
-	"github.com/go-park-mail-ru/2025_1_ChillGuys/config"
 	"net/http"
+
+	"github.com/go-park-mail-ru/2025_1_ChillGuys/config"
 )
 
 func CORSMiddleware(next http.Handler, conf *config.ServerConfig) http.Handler {
@@ -14,18 +15,18 @@ func CORSMiddleware(next http.Handler, conf *config.ServerConfig) http.Handler {
 			return
 		}
 
-		//allowedOrigin := conf.AllowOrigin
-		allowedOrigin := "*"
+		w.Header().Set("Access-Control-Allow-Methods", conf.AllowMethods)
+		w.Header().Set("Access-Control-Allow-Headers", conf.AllowHeaders)
+		w.Header().Set("Access-Control-Allow-Credentials", conf.AllowCredentials)
+		w.Header().Set("Access-Control-Allow-Origin", conf.AllowOrigin)
+
+		allowedOrigin := conf.AllowOrigin
+		//allowedOrigin := "*"
 
 		if allowedOrigin != "*" && allowedOrigin != origin {
 			http.Error(w, "CORS Origin not allowed", http.StatusForbidden)
 			return
 		}
-
-		w.Header().Set("Access-Control-Allow-Methods", conf.AllowMethods)
-		w.Header().Set("Access-Control-Allow-Headers", conf.AllowHeaders)
-		w.Header().Set("Access-Control-Allow-Credentials", conf.AllowCredentials)
-		w.Header().Set("Access-Control-Allow-Origin", conf.AllowOrigin)
 
 		next.ServeHTTP(w, r)
 	})
