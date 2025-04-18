4b81f0d4f2f26b93765b70fd910dfdb2f61ade38
Nikita <nmamaev49@gmail.com>
2025-04-18 22:29:44 +0300

    fix cors

--- internal/transport/middleware/cors.go
+++ internal/transport/middleware/cors.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-park-mail-ru/2025_1_ChillGuys/config"
@@ -9,25 +8,22 @@ import (
 
 func CORSMiddleware(next http.Handler, conf *config.ServerConfig) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+
+		if origin == "" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", conf.AllowMethods)
 		w.Header().Set("Access-Control-Allow-Headers", conf.AllowHeaders)
 		w.Header().Set("Access-Control-Allow-Credentials", conf.AllowCredentials)
 		w.Header().Set("Access-Control-Allow-Origin", conf.AllowOrigin)
-		
-		origin := r.Header.Get("Origin")
-
-		fmt.Println(origin + " cors")
-		if origin == "" {
-			fmt.Println("return if origin")
-			next.ServeHTTP(w, r)
-			return
-		}
 
 		allowedOrigin := conf.AllowOrigin
 		//allowedOrigin := "*"
 
 		if allowedOrigin != "*" && allowedOrigin != origin {
-			fmt.Println(allowedOrigin + "!=")
 			http.Error(w, "CORS Origin not allowed", http.StatusForbidden)
 			return
 		}
